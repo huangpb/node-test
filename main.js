df3b7b38dfb74bbc1013bf9d7db7cc732c14fc23
黄攀斌 <yimi.hpb@raycloud.com>
2019-10-23 16:13:27 +0800

    fix

--- main.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html; charset=utf8'});
-
-    res.write('<h2>222</h2>')
-    res.end();
-}).listen(5555);
