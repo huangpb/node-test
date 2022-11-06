cb2a3c4866fa49c1ab6475c70fb3c625f4925718
黄攀斌 <yimi.hpb@raycloud.com>
2022-11-06 18:40:20 +0800

    get、post 请求的通用化 mock

--- express_demo.js
+++ express_demo.js
@@ -1,11 +1,46 @@
 var express = require('express');
 var fs = require('fs');
+var path = require('path');
 
 var app = express();
 
-app.get('/v1/home', function(req, res) {
-    fs.readFile(__dirname + '/user.json', 'utf8', function(err, data) {
-        // data = JSON.parse(data);
+function dirPath(url = '') {
+    const fileName = url.split('/').filter(Boolean).join('-') + '.json';
+    return path.resolve(__dirname, 'mock', fileName);
+}
+
+//设置跨域访问（设置在所有的请求前面即可）
+app.all("*", function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "*");
+	res.header("Access-Control-Allow-Methods", "DELETE,PUT,POST,GET,OPTIONS");
+	if (req.method == 'OPTIONS')
+		res.sendStatus(200); //让options尝试请求快速结束
+	else
+		next();
+});
+
+// app.get('/v1/home', function(req, res) {
+//     fs.readFile(__dirname + '/mock/user.json', 'utf8', function(err, data) {
+//         data = JSON.parse(data);
+
+//         // var user = data['user' + req.params.id];
+//         res.json(data);
+//         res.end();
+//     })
+// })
+
+// app.post('/security/login', function(req, res) {
+//     fs.readFile(__dirname + '/mock/login.json', 'utf8', function(err, data) {
+//         data = JSON.parse(data);
+//         res.json(data);
+//         res.end();
+//     })
+// })
+
+app.get('/*', function(req, res) {
+    fs.readFile(dirPath(req.path), 'utf8', function(err, data) {
+        data = JSON.parse(data);
 
         // var user = data['user' + req.params.id];
         res.json(data);
@@ -13,15 +48,16 @@ app.get('/v1/home', function(req, res) {
     })
 })
 
-app.post('/security/login', function(req, res) {
-    fs.readFile(__dirname + '/login.json', 'utf8', function(err, data) {
+app.post('/*', function(req, res) {
+    fs.readFile(dirPath(req.path), 'utf8', function(err, data) {
+        data = JSON.parse(data);
         res.json(data);
         res.end();
     })
 })
 
 app.listen(8081, function() {
-    console.log('http://localhost:8081')
+    console.log('server run in http://localhost:8081')
 });
 
 
